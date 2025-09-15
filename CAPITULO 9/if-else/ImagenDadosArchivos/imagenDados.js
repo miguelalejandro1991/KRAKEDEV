680c5967b9ab8b9fa3ec7259b48b5ceb74636e22
miguelalejandro1991 <mibitech22@gmail.com>
2025-09-14 19:35:07 -0500

    dados inicio

---
+++ CAPITULO 9/if-else/ImagenDadosArchivos/imagenDados.js
@@ -0,0 +1,20 @@
+jugar=function(){
+    let resultado;
+    resultado=lanzarDado();
+    console.log(resultado);
+   
+}
+
+
+
+lanzarDado=function(){
+    let aleatorio;
+    let aleatorioMultiplicado;
+    let aleatorioEntero;
+    let valorDado;
+    aleatorio=Math.random();
+    aleatorioMultiplicado=aleatorio*6;
+    aleatorioEntero=parseInt(aleatorioMultiplicado);
+    valorDado=aleatorioEntero+1;
+    return valorDado;
+}
