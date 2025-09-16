a157ebbba00579180157f08f99e2339511e6fd29
miguelalejandro1991 <mibitech22@gmail.com>
2025-09-16 12:42:10 -0500

    el boton modifica la imagen

--- MODULO 1 JS/1. manejo de versiones con Git/ejercicio git/ejercicio.js
+++ MODULO 1 JS/1. manejo de versiones con Git/ejercicio git/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/excelente.gif";
+
     console.log("ingresa a saludar");
 }
