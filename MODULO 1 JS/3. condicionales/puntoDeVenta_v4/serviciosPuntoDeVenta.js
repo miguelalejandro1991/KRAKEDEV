33604a24f02b31dbb9ad0be721a654a8cf714664
miguelalejandro1991 <mibitech22@gmail.com>
2025-10-01 11:04:26 -0500

    punto de venta final

--- MODULO 1 JS/3. condicionales/puntoDeVenta_v4/serviciosPuntoDeVenta.js
+++ MODULO 1 JS/3. condicionales/puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -1,30 +1,66 @@
-calcularValorDescuento=function(monto, porcentajeDescuento){
-    //Calcula el valor del porcentaje de descuento.
-    let valorDescuento= (monto*porcentajeDescuento)/100;
-    return valorDescuento;
+let valorSubtotal;
+let valorDescuento;
+let valorIVA;
+let valorTotal;
 
-}
+const string_required = 'Campo obligatorio'
+const max_value = 'Max. 10 caracteres'
+const cantidad_max = 'Max. 100 productos'
 
-calcularlVA=function(monto){
-    //retorna el valor del IVA
-    let valorIVA=(monto*1.15);
+const calcularIVA = (monto) => {
+    const valorIVA = (monto * 12) / 100
     return valorIVA;
+};
+const calcularSubTotal = (precio, cantidad) => {
+    const totalPagar = precio * cantidad;
+    return totalPagar;
+};
+
+const calcularTotal = (subTotal, descuento, iva) => {
+    const totalPagar = subTotal - descuento + iva
+    return totalPagar;
+};
+
+const calcularDescuentoPorVolumen = (subTotal, cantidad) => {
+    let descuento;
+    let res;
+    if (cantidad < 3) {
+        descuento = 0;
+    } else if (cantidad >= 3 && cantidad <= 5) {
+        descuento = 3;
+    } else if (cantidad >= 6 && cantidad <= 11) {
+        descuento = 4;
+    } else {
+        descuento = 5;
+    }
+    return res = subTotal * descuento / 100
+
 }
 
-calcularSubtotal=function(precio, cantidad){
-    //Calcula el total a pagar, que es la multiplicación del precio unitario por la cantidad de productos
-    // RetornaEl total a pagar del número de productos comprados
 
-    let subtotal=precio*cantidad;
-    return subtotal; 
-
+const esProductoValido = (nombreProducto) => {
+    if (!nombreProducto) {
+        document.getElementById('field_product').innerText = string_required
+    } else {
+        document.getElementById('field_product').innerText = ''
+    }
+    if (nombreProducto.length > 10) {
+        document.getElementById('field_product').innerText = max_value
+    }
 }
 
-calcularTotal=function(subtotal, descuento, iva){
-    //Calcula el valor total a pagar, que sería el subtotal, menos el descuento, más el IVA.
-    let total=subtotal-descuento+iva;
-    return total;
+const esCantidadValida = (cantidad) => {
+    if (!cantidad) {
+        document.getElementById('field_amount').innerText = string_required
+    } else {
+        document.getElementById('field_amount').innerText = ''
+    }
+
+    if (cantidad > 0 && cantidad <= 100) {
+        document.getElementById('field_amount').innerText = ''
+    } else {
+        document.getElementById('field_amount').innerText = cantidad_max
+    }
 
 
 }
-
