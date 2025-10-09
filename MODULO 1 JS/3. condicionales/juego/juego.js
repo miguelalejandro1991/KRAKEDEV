2ca60b01d087a62f136d48176407e02e276ce2b4
miguelalejandro1991 <mibitech22@gmail.com>
2025-10-09 02:52:16 -0500

    juego inicio

---
+++ MODULO 1 JS/3. condicionales/juego/juego.js
@@ -0,0 +1,12 @@
+/* agregar la funcion jugar, que recibe como parametro un string llamado seleccionado. la funcion realiza lo siguiente:
+1. invoca a gerarElemento y guarda el resultado en una variable.
+2. con la variable obtenida invoca a generarRuta y con este valor muestra la imagen seleccionada por el computador.
+3. invoca a determinarGanador pasandole el valor que obtuvo en generarElemento y el valor que recibe la funcion en el
+parametro seleccionado. si el resultado de determinarGanador es 0, muestra el mensaje "empate", si gana el usuario 
+muestra "ganaste" y si gana el computador muestra "perdiste".*/
+
+jugar=function(seleccionado){
+
+}
+
+
---
+++ MODULO 1 JS/3. condicionales/juego/serviciosJuego.js
@@ -0,0 +1,33 @@
+/*crear la funcion obtenerAleatorio que obtiene un numero aleatorio entero entre 1 al 3, retorna 
+un numero entero randomico entre 1 y 3, ambos incluidos*/
+
+obtenerAleatorio=function(){
+
+}
+
+/* crear la funcion generarElemento que genera randomicamente las cadenas "piedra", "papel" o "tijera".
+algoritmo: genera un numero randomico de 1 al 3, invocando a la funcion obtenerAleatorio.
+si el numero es 1, retorna "piedra"
+si el numero es 2, retorna "papel"
+si el numero es 3, retorna "tijera" */
+
+generarElemento=function(){
+
+}
+
+/* crear la funcion determinar ganador, determina cual de los dos jugadores gano la ronda.
+retorna 0 si empatan, 1 si gana el jugador 1, 2 si gana el jugador 2*/
+
+determinarGanador=function(jugador1, jugador2){
+
+}
+
+/* crear la funcion generarRuta, genera la ruta de la imagen, en base al nombre que recibe, la ruta es
+./imagenes/<nombre>.png.
+retorna la ruta de la imagen que tiene que pintar*/
+
+generarRuta=function(nombre){
+
+}
+
+
