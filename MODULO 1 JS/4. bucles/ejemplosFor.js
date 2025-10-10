e7821a3f9ca41090138ad17bef871ee523623ade
miguelalejandro1991 <mibitech22@gmail.com>
2025-10-10 09:11:06 -0500

    reto 37

---
+++ MODULO 1 JS/4. bucles/ejemplosFor.js
@@ -0,0 +1,43 @@
+mostrarNumeros=function(){
+    console.log("antes del for");
+    for(let i=0;i<4;i++){
+        console.log(i);
+    }
+
+
+    console.log("despues del for");
+}
+
+mostrarNumeros2=function(){
+    console.log("antes del for");
+    for(let indice=0;indice<=5;indice++){
+        console.log(indice);
+    }
+    console.log("despues del for");
+}
+mostrarNumerosPares=function(){
+    console.log("antes del for");
+    for(let x=2;x<=10;x+=2){
+        console.log(x);
+    }
+}
+mostrarInverso=function(){
+    console.log("antes del for");
+    for(let j=10;j>=0;j--){
+        console.log(j);
+    }
+}
+hackearNasaPelis=function(){
+    //hackeando nada 0%
+    for(let porcentaje=0;porcentaje<=100;porcentaje++){
+        console.log("hackeando Nasa "+porcentaje+"%");
+    }
+    console.log("Nasa hackeada");
+}
+mostrarImpares=function(){
+    console.log("antes del for");
+    for(let y=1;y<=21;y+=2){
+        console.log(y);
+    }
+    console.log("despues del for");
+}
