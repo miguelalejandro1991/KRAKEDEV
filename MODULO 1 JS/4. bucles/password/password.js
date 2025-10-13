d54e9104f02e60401aff6206545b181129ab6bdb
miguelalejandro1991 <mibitech22@gmail.com>
2025-10-13 09:37:11 -0500

    reto 39

--- MODULO 1 JS/4. bucles/password/password.js
+++ MODULO 1 JS/4. bucles/password/password.js
@@ -13,13 +13,65 @@ lo mismo con un digito y el caracter especial.
 Retorna el mensaje con los errores de validacion. si todo es correcto retorna una cadena vacia(cadena de longitud 0)*/
 
 
-ejecutarValidacion=function(){
 
 
-    
+
+validarPassword = function (password) {
+
+    let mensaje = "";
+
+    if (password.length < 8) {
+        mensaje += "la contraseña debe tener al menos 8 caracteres <br>";
+    }
+    if (password.length > 16) {
+        mensaje += "la contraseña debe tener como maximo 16 caracteres <br>";
+    }
+    let tieneMayuscula = false;
+    let tieneDigito = false;
+    let tieneEspecial = false;
+    let especiales = "*-_";
+    for (let i = 0; i < password.length; i++) {
+        let char = password.charAt(i);
+        if (char >= 'A' && char <= 'Z') {
+            tieneMayuscula = true;
+        }
+        if (char >= '0' && char <= '9') {
+            tieneDigito = true;
+        }   
+        if (especiales.indexOf(char) !== -1) {
+            tieneEspecial = true;
+        }
+    }
+    if (!tieneMayuscula) {
+        mensaje += "la contraseña debe tener al menos una letra mayuscula <br>";
+    }       
+    if (!tieneDigito) {
+        mensaje += "la contraseña debe tener al menos un digito <br>";
+    }   
+    if (!tieneEspecial) {
+        mensaje += "la contraseña debe tener al menos un caracter especial (*, -, _) <br>";
+    }   
+    return mensaje;
+        
+
+
 }
 
+/* crear la fun cion ejecutarValidacion que se ejecuta al hacer click en el boton validar.
+recupera la caja de texto de la pagina. invocar a validarPassword, si el retorno es cadena vacia, mostrar un mensaje 
+password correcto, caso contrario mostrar los mensajes de errores de validacion. */
 
-validarPassword=function(password){
+
+ejecutarValidacion = function () {
+    console.log("estoy dentro")
+    let password = document.getElementById("txtpass").value;
+    console.log(password);
+    let mensaje = validarPassword(password);
+    if (mensaje.length == 0) {
+        document.getElementById("errores").innerHTML = "contraseña correcta ";
+    } else {
+        document.getElementById("errores").innerHTML = mensaje;
+    }
+
 
 }
