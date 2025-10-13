df249f91a235d8211bcf86c8dc35000899f180b2
miguelalejandro1991 <mibitech22@gmail.com>
2025-10-13 08:50:55 -0500

    busqueda en cadena

--- MODULO 1 JS/4. bucles/cadena/ejercicioCadenas.js
+++ MODULO 1 JS/4. bucles/cadena/ejercicioCadenas.js
@@ -1,6 +1,6 @@
 ejecutarPrueba1 = function () {
     let mensaje
-    mensaje=recuperarTexto("txtCadena");
+    mensaje = recuperarTexto("txtCadena");
     recorrerCadena(mensaje);
 
 }
@@ -10,15 +10,15 @@ recorrerCadena = function (cadena) {
 
     let caracter;
 
-    for (let posicion = 0; posicion<cadena.length; posicion++) {
-        caracter=cadena.charAt(posicion);
-        console.log("caracter "+posicion+"posicion: "+posicion);
+    for (let posicion = 0; posicion < cadena.length; posicion++) {
+        caracter = cadena.charAt(posicion);
+        console.log("caracter " + posicion + "posicion: " + posicion);
 
     }
-    
-    for(let posicion=0;posicion<=cadena.length-1;posicion++){
-        caracter=cadena.charAt(posicion);
-        console.log("CARACTER "+posicion+"posicion: "+posicion);
+
+    for (let posicion = 0; posicion <= cadena.length - 1; posicion++) {
+        caracter = cadena.charAt(posicion);
+        console.log("CARACTER " + posicion + "posicion: " + posicion);
 
     }
 }
@@ -27,19 +27,45 @@ recorrerCadena = function (cadena) {
 //juan -> nauj
 //ana -> ana
 //tambien que la imprima en pantalla en el h2 resultado
-ejecutarPrueba2=function(){
-    
+ejecutarPrueba2 = function () {
+
     let mensaje
-    mensaje=recuperarTexto("txtCadena");
-    let cadenaInvertida=invertirCadena(mensaje);
-    mostrarTexto("resultado",cadenaInvertida);
+    mensaje = recuperarTexto("txtCadena");
+    let cadenaInvertida = invertirCadena(mensaje);
+    mostrarTexto("resultado", cadenaInvertida);
 }
-invertirCadena=function(cadena){
-    
+invertirCadena = function (cadena) {
+
     let caracter;
-    for(let posicion=cadena.length-1;posicion>=0;posicion--){
-        caracter+=cadena.charAt(posicion);
-        
+    for (let posicion = cadena.length - 1; posicion >= 0; posicion--) {
+        caracter += cadena.charAt(posicion);
+
     }
     return caracter;
+}
+
+buscarLetra = function (cadena, letra) {
+    let letraIterada;
+    let existeLetra = false;
+    for (let i = 0; i < cadena.length; i++) {
+        letraIterada = cadena.charAt(i);
+        if (letraIterada == letra) {
+            existeLetra = true;
+        }
+    }
+    if (existeLetra == true) {
+        return true;
+        console.log("existe");
+    }else{
+        return false;
+        console.log("no existe")
+    }
+
+}
+
+contarMayusculas=function(cadena){
+    let letra;
+    for(let i=0; i<cadena.length;i++){
+        letra=cadena.charAt(i);
+    }
 }
