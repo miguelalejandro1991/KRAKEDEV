6d1d619d3e0fdc65e584b784a7898cb12b9db475
miguelalejandro1991 <mibitech22@gmail.com>
2025-10-15 11:58:54 -0500

    reto44

--- MODULO 1 JS/6.objetos/clientes.js
+++ MODULO 1 JS/6.objetos/clientes.js
@@ -4,13 +4,91 @@ let clientes = [
     { cedula: "45645648", nombre: "grace", edad: "28" }
 ];
 
+guardarCambios = function(){    
+    let valorCedula = recuperarTexto("txtCedula");
+    let valorNombre = recuperarTexto("txtNombre");
+    let valorEdad = recuperarFloat("txtEdad");
+    let datosCliente ={};
+    datosCliente.cedula=valorCedula;
+    datosCliente.nombre=valorNombre;
+    datosCliente.edad=valorEdad;
+    modificarCliente(datosCliente);
+    mostrarClientes();
+
+    
+}
+
+modificarCliente = function (cliente) {
+    let clienteEncontrado=buscarCliente(cliente.cedula);
+    if(clienteEncontrado!=null){
+        clienteEncontrado.nombre=cliente.nombre;
+        clienteEncontrado.edad=cliente.edad;
+        
+    }
+
+}
+
+agregarCliente = function (cliente) {
+    let resultado;
+    resultado = buscarCliente(cliente.cedula);
+    if (resultado == null) {
+        clientes.push(cliente);
+    }else{
+        alert("ya existe el cliente con la cedula:" +cliente.cedula);
+    }
+
+}
+
+crearCliente=function(){
+    let valorCedula = recuperarTexto("txtCedula");
+    let valorNombre = recuperarTexto("txtNombre");
+    let valorEdad = recuperarFloat("txtEdad");
+    let nuevoCliente ={}
+        nuevoCliente.cedula=valorCedula;
+        nuevoCliente.nombre=valorNombre;
+        nuevoCliente.edad=valorEdad;
+        
+        agregarCliente(nuevoCliente);
+    
+
+
+}
+
+buscarCliente = function (cedula) {
+    let elementoCliente;
+    let clienteEncontrado = null;
+    for (let i = 0; clientes.length; i++) {
+        elementoCliente = clientes[i];
+        if (elementoCliente.cedula == cedula) {
+            clienteEncontrado = elementoCliente;
+            break;
+        }
+
+    }
+    return clienteEncontrado;
+}
+
+ejecutarBusqueda = function () {
+    let valorCedula = recuperarTexto("txtCedulaBuscar");
+    let cliente = buscarCliente(valorCedula);
+    if(respuesta == null){
+        alert ("cliente no encontrado");
+    }else{
+        mostrarTextoEnCaja("txtCedula", cliente.cedula);
+        mostrarTextoEnCaja("txtNombre", cliente.nombre);
+        mostrarTextoEnCaja("txtEdad", cliente.edad);
+
+    }
+}
+
+
 mostrarClientes = function () {
-    let cmpTabla=document.getElementById("tablaClientes"); 
-    let contenidoTabla = "<table><tr>"+
-    "<th>CEDULA</th>"+
-    "<th>NOMBRE</th>"+
-    "<th>EDAD</th>"+
-    "</tr>";
+    let cmpTabla = document.getElementById("tablaClientes");
+    let contenidoTabla = "<table><tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>EDAD</th>" +
+        "</tr>";
     let elementoCliente;
     for (let i = 0; i < clientes.length; i++) {
         elementoCliente = clientes[i];
@@ -20,6 +98,6 @@ mostrarClientes = function () {
             + "<td>" + elementoCliente.edad + "</td>"
             + "</tr>"
     }
-    contenidoTabla+="</table>"
-    cmpTabla.innerHTML=contenidoTabla;
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML = contenidoTabla;
 }
