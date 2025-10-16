41619e9b052cc718c34a4d49a1b6615dbd2b5425
miguelalejandro1991 <mibitech22@gmail.com>
2025-10-16 15:01:43 -0500

    reto 54 inicio

---
+++ MODULO 1 JS/Miguel Rodriguez/orden.js
@@ -0,0 +1,11 @@
+let = [
+    { identificacion: 0, nombre: "Marcos", edad: "18", },
+    { identificacion: 1, nombre: "Roberto", edad: "15", },
+    { identificacion: 2, nombre: "Kate", edad: "25", },
+    { identificacion: 3, nombre: "Diana", edad: "12", },
+    { identificacion: 4, nombre: "Benja", edad: "5", },
+
+
+
+
+]
